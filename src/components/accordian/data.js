b18f2aa10ce797bf390e26257ec9488c154c2410
Tanzim <tanzim008@gmail.com>
2024-02-25 16:51:50 -0330

    first commit

---
+++ src/components/accordian/data.js
@@ -0,0 +1,18 @@
+const data = [
+    {
+        id:1,
+        question: "SPACE AND SIZE OF HERITAGE ROOMS?",
+        answer: "Heritage has been functioning in a separate building of 510sqm. The length of Heritage collection would be approximately 2.5 km.",
+    },
+    {
+        id:2,
+        question: "MANPOWER?",
+        answer: "It has no permanent staff. The founder himself and some temporary staff work here.",
+    },
+    {
+        id:3,
+        question: "RESEARCH FACILITIES?",
+        answer: "It currently remains open 3 & a half hours a day, from 9.00am to 12.30Pm. Three researchers can work together. For financial constraints it can not provide much facilities to the researchers.",
+    }
+];
+export default data;
