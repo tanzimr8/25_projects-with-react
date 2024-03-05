4c12285fa9151279dd9f69247d73353465853efb
Tanzim <tanzim008@gmail.com>
2024-03-05 02:28:54 -0330

    load more project completed

--- src/components/LoadMore/index.jsx
+++ src/components/LoadMore/index.jsx
@@ -5,18 +5,18 @@ const LoadMore = () => {
     const [products,setProducts] = useState([]);
     const [count, setCount] = useState(0);
     const [errMsg,setErrMsg] = useState('');
+    const [disable,setDisable] =useState(false);
     async function fetchProducts(){
         try{
             setLoading(true);
             const response = await fetch(`https://dummyjson.com/products?limit=20&skip=${ count===0 ? 0: count*20}`);
             const result = await response.json();
             if(result && result.products && result.products.length) {
-                setProducts(result.products);
+                setProducts(prevData => [...prevData,...result.products]);
                 setLoading(false);
             }
             
         }
-        
         catch(e){
             setErrMsg(e);
             setLoading(false);
@@ -26,9 +26,15 @@ const LoadMore = () => {
     console.log(products);
     useEffect(()=>{
         fetchProducts();
-    },[]);
+    },[count]);
+    
+    useEffect(()=>{
+        if(products.length === 100){
+            setDisable(true);
+        }
+    },[products]);
     if(loading){
-        return 'No data to show';
+        return <h2>LOADING...</h2>;
     }
     return (
         <section>
@@ -42,6 +48,12 @@ const LoadMore = () => {
                     })
                 
                 : null}
+            </div>
+            <div className="loadmore">
+                <button disabled={disable} onClick={()=>{setCount(count+1)}} className="btn btn-primary my-5" >Load More</button>
+                    {
+                        disable ? <p>You have reached 100 products</p> : ''
+                    }
             </div>
         </section>
     )
