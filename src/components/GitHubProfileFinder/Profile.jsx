a81e3bcad261cc00ff5318b870cb8a256b589896
Tanzim <tanzim008@gmail.com>
2024-03-23 00:45:11 -0230

    GithubProfileViewerBeta

--- src/components/GitHubProfileFinder/Profile.jsx
+++ src/components/GitHubProfileFinder/Profile.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBBtn, MDBTypography, MDBIcon } from 'mdb-react-ui-kit';
 
-export default function Profile() {
+export default function Profile({ user }) {
+  const { login,avatar_url, url, type, name, blog, location, bio, public_repos, followers, following, created_at } = user;
   return (
     <div className="vh-100" style={{ backgroundColor: '#eee' }}>
       <MDBContainer className="container py-5 h-100">
@@ -10,14 +11,14 @@ export default function Profile() {
             <MDBCard style={{ borderRadius: '15px' }}>
               <MDBCardBody className="text-center">
                 <div className="mt-3 mb-4">
-                  <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava2-bg.webp"
+                  <MDBCardImage src={avatar_url}
                     className="rounded-circle" fluid style={{ width: '100px' }} />
                 </div>
-                <MDBTypography tag="h4">Julie L. Arsenault</MDBTypography>
+                <MDBTypography tag="h4">{name}</MDBTypography>
                 <MDBCardText className="text-muted mb-4">
-                  @Programmer <span className="mx-2">|</span> <a href="#!">mdbootstrap.com</a>
+                  {type} <span className="mx-2">|</span> <a href={blog}>Website</a>
                 </MDBCardText>
-                <div className="mb-4 pb-2">
+                {/* <div className="mb-4 pb-2">
                   <MDBBtn outline floating>
                     <MDBIcon fab icon="facebook" size="lg" />
                   </MDBBtn>
@@ -27,22 +28,21 @@ export default function Profile() {
                   <MDBBtn outline floating>
                     <MDBIcon fab icon="skype" size="lg" />
                   </MDBBtn>
-                </div>
-                <MDBBtn rounded size="lg">
-                  Message now
-                </MDBBtn>
+                </div> */}
+                  <a target='_blank' href={`https://github.com/${login}`}>View Profile</a>
+                
                 <div className="d-flex justify-content-between text-center mt-5 mb-2">
                   <div>
-                    <MDBCardText className="mb-1 h5">8471</MDBCardText>
-                    <MDBCardText className="small text-muted mb-0">Wallets Balance</MDBCardText>
+                    <MDBCardText className="mb-1 h5">{public_repos}</MDBCardText>
+                    <MDBCardText className="small text-muted mb-0">Total Repo</MDBCardText>
                   </div>
                   <div className="px-3">
-                    <MDBCardText className="mb-1 h5">8512</MDBCardText>
+                    <MDBCardText className="mb-1 h5">{followers}</MDBCardText>
                     <MDBCardText className="small text-muted mb-0">Followers</MDBCardText>
                   </div>
                   <div>
-                    <MDBCardText className="mb-1 h5">4751</MDBCardText>
-                    <MDBCardText className="small text-muted mb-0">Total Transactions</MDBCardText>
+                    <MDBCardText className="mb-1 h5">{following}</MDBCardText>
+                    <MDBCardText className="small text-muted mb-0">Following</MDBCardText>
                   </div>
                 </div>
               </MDBCardBody>
--- src/components/GitHubProfileFinder/index.jsx
+++ src/components/GitHubProfileFinder/index.jsx
@@ -1,21 +1,47 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Profile from './Profile';
 
 const GitHubProfileFinder = () => {
-  const [users,setUsers] = useState([]);
-  const [inputUser,setInputUser] = useState('');
-  const [loading,setLoading] = useState(false);
+  const [users, setUsers] = useState(null);
+  const [inputUser, setInputUser] = useState('tanzimr8');
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = ()=>{
+    fetchGitUsers();
+  }
+  const fetchGitUsers = async () => {
+    try{
+      setLoading(true);
+      const url = `https://api.github.com/users/${inputUser}`
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      if(data){
+        setUsers(data);
+        setLoading(false);
+        setInputUser('');
+      }
+    }
+    catch(e){
+      console.log(e);
+    }
+
+  }
+  useEffect(() => {
+    fetchGitUsers();
+  }, []);
+  if(loading){
+    <h2>Please wait...</h2>
+  }
   return (
     <>
       <div className='search-profile'>
         <label htmlFor="user">Search github profile</label>
-        <input type="text" name="user" id="user" placeholder='search user...' />
+        <input type="text" name="user" id="user" placeholder='search user...' value={inputUser} onChange={(e) => { setInputUser(e.target.value) }} />
+        <button className='btn' type='submit' onClick={handleSubmit}>Submit</button>
       </div>
       <div className="search-result">
-
-      </div>
-      <div>
-        <Profile/>
+      {users != null ? <Profile user = {users}/> : null} 
+    
       </div>
     </>
   )
